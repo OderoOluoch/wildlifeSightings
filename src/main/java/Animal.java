5cb728ef459a7f216c19a24f5bf45462af76ec9f
OderoOluoch <oderoluoch@gmail.com>
2021-06-12 11:26:23 +0300

    Animal class in place

---
+++ src/main/java/Animal.java
@@ -0,0 +1,82 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Animal {
+    public String name;
+    public int id;
+
+    public Animal(String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal) {
+        if(!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+        }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name) VALUES (:name);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Animal> all() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals;";
+            return con.createQuery(sql)
+                    .executeAndFetch(Animal.class);
+        }
+    }
+
+    public static Animal find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals WHERE id=:id;";
+            Animal animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Animal.class);
+            return animal;
+        }
+    }
+
+    public void updateName(String name) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE animals SET name=:name WHERE id=:id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .addParameter("name", name)
+                    .executeUpdate();
+        }
+    }
+
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals WHERE id=:id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    //Sighting class will be needed to complete this.
+
+}
+
